753ec89827c73b8ee0bf6794128fbab0ff3bcb5b
Graham Cockell <graham.cockell@outlook.com>
2019-11-13 11:05:11 +0000

    Refactor

--- main.py
+++ main.py
@@ -1,14 +1,135 @@
 import datetime
 import json
+from time import sleep
 
 import sqlalchemy as sql
 
 import betfairlightweight as bfl
 
-from models import Event, Market, Runner
+from models import Event, Market, Runner, MarketBook, RunnerBook
+
+
+def get_events(db_session, betfair_api, date):
+
+    events = []
+    bfl_event_results = betfair_api.betting.list_events(
+        filter = bfl.filters.market_filter(
+            event_type_ids = [7],
+            market_countries = ['GB', 'IE'],
+            market_type_codes=['WIN'],
+            market_start_time={
+                'from': date.strftime("%Y-%m-%dT%TZ"),
+                'to': (date + datetime.timedelta(days=1)).strftime("%Y-%m-%dT%TZ"),
+            },
+        )
+    )
+    for bfl_event_result in bfl_event_results:
+        bfl_event = bfl_event_result.event
+        event = db_session.query(Event).filter(Event.betfair_id==bfl_event.id).one_or_none()
+        if not event:
+            event = Event(
+                betfair_id=bfl_event.id,
+                name=bfl_event.name,
+                country_code=bfl_event.country_code,
+                time_zone=bfl_event.time_zone,
+                venue=bfl_event.venue,
+                open_date=bfl_event.open_date
+            )
+            db_session.add(event)
+        events.append(event)
+        db_session.commit()
+    return events
+
+
+def get_markets(db_session, betfair_api, event):
+
+    markets = []
+    bfl_markets = betfair_api.betting.list_market_catalogue(
+        filter = bfl.filters.market_filter(
+            event_ids = [event.betfair_id],
+            market_type_codes=['WIN'],
+        ),
+        max_results=25,
+        market_projection=['MARKET_START_TIME', 'RUNNER_DESCRIPTION']
+    )
+    for bfl_market in bfl_markets:
+        market = db_session.query(Market).filter(Market.betfair_id==bfl_market.market_id).one_or_none()
+        if not market:
+            market = Market(
+                event_id=event.id,
+                betfair_id=bfl_market.market_id,
+                name=bfl_market.market_name,
+                start_time=bfl_market.market_start_time,
+                total_matched=bfl_market.total_matched
+            )
+            db_session.add(market)
+            for bfl_runner in bfl_market.runners:
+                runner = db_session.query(Runner).filter(Runner.betfair_id==bfl_runner.selection_id).one_or_none()
+                if not runner:
+                    runner = Runner(
+                        market_id=market.id,
+                        betfair_id=bfl_runner.selection_id,
+                        name=bfl_runner.runner_name,
+                        sort_priority=bfl_runner.sort_priority
+                    )
+                    db_session.add(runner)
+        markets.append(market)
+        db_session.commit()
+    return markets
+
+
+def get_market_book(db_session, betfair_api, market):
+
+    market_book = None
+    #print(f"  {datetime.datetime.now()}: Sending API request")
+    bfl_market_books = betfair_api.betting.list_market_book([market.betfair_id])
+    #print(f"  {datetime.datetime.now()}: API response received")
+    if bfl_market_books:
+        bfl_market_book = bfl_market_books[0]
+        market_book = MarketBook(
+            market_id = market.id,
+            date_time = datetime.datetime.now(),
+            delayed = bfl_market_book.is_market_data_delayed,
+            status = bfl_market_book.status,
+            bet_delay = bfl_market_book.bet_delay,
+            bsp_reconciled = bfl_market_book.bsp_reconciled,
+            complete = bfl_market_book.complete,
+            inplay = bfl_market_book.inplay,
+            number_of_winners = bfl_market_book.number_of_winners,
+            number_of_runners = bfl_market_book.number_of_runners,
+            number_of_active_runners = bfl_market_book.number_of_active_runners,
+            last_match_time = bfl_market_book.last_match_time,
+            total_matched = bfl_market_book.total_matched,
+            total_available = bfl_market_book.total_available,
+            cross_matching = bfl_market_book.cross_matching,
+            runners_voidable = bfl_market_book.runners_voidable,
+            version = 0 #bfl_market_book.version
+        )
+        db_session.add(market_book)
+        #print(f"  {datetime.datetime.now()}: Market book created")
+        for bfl_runner_book in bfl_market_book.runners:
+            runner = db_session.query(Runner).filter(Runner.betfair_id==bfl_runner_book.selection_id).one_or_none()
+            if runner:
+                runner_book = RunnerBook(
+                    market_book_id = market_book.id,
+                    runner_id = runner.id,
+                    handicap = bfl_runner_book.handicap,
+                    status = bfl_runner_book.status,
+                    adjustment_factor = bfl_runner_book.adjustment_factor,
+                    last_price_traded = bfl_runner_book.last_price_traded,
+                    total_matched = bfl_runner_book.total_matched,
+                    removal_date = bfl_runner_book.removal_date
+                )
+                db_session.add(runner_book)
+        #print(f"  {datetime.datetime.now()}: Runner books created")
+        db_session.commit()
+        #print(f"  {datetime.datetime.now()}: DB changes committed")
+    return market_book
+
 
 # DB connection URL
-SQLALCHEMY_URL = 'postgresql://postgres:barnum@qnap:32768/betfairlogger'
+#SQLALCHEMY_URL = 'postgresql://postgres:barnum@qnap:32768/betfairlogger'
+SQLALCHEMY_URL = 'postgresql://postgres:barnum@localhost/betfairlogger'
 
 # Read secrets
 with open('secrets.json') as f:
@@ -22,58 +143,68 @@ Session = sql.orm.sessionmaker(bind=db_engine)
 betfair_api = bfl.APIClient(
     secrets['username'],
     secrets['password'],
-    app_key=secrets['app_key'],
+    app_key=secrets['betangel_app_key'],
     cert_files=['./certs/client-2048.crt', './certs/client-2048.key'],
     lightweight=False
 )
 betfair_api.login()
 
-# Create/update GB and IRE horse racing events for today
-events = betfair_api.betting.list_events(
-    filter = bfl.filters.market_filter(
-        event_type_ids = [7],
-        market_countries = ['GB', 'IE'],
-        market_type_codes=['WIN'],
-        market_start_time={
-            'from': datetime.datetime.today().strftime("%Y-%m-%dT%TZ"),
-            'to': (datetime.datetime.today() + datetime.timedelta(days=1)).strftime("%Y-%m-%dT%TZ"),
-        },
-    )
-)
+# Create DB session
 db_session = Session()
-for event in events:
-    event = event.event
-    print(event.name)
-    db_event = db_session.query(Event).filter(Event.betfair_id==event.id).one_or_none()
-    if not db_event:
-        db_event = Event(betfair_id=event.id, name=event.name, venue=event.venue)
-        db_session.add(db_event)
-        db_session.commit()
-    markets = betfair_api.betting.list_market_catalogue(
-        filter = bfl.filters.market_filter(
-            event_ids = [event.id],
-            market_type_codes=['WIN'],
-        ),
-        max_results=10,
-        market_projection=['MARKET_START_TIME', 'RUNNER_DESCRIPTION']
-    )
-    for market in markets:
-        print(f'  {market.market_name}')
-        db_market = db_session.query(Market).filter(Market.betfair_id==market.market_id).one_or_none()
-        if not db_market:
-            db_market = Market(event_id=db_event.id, betfair_id=market.market_id, name=market.market_name, start_time=market.market_start_time, total_matched=market.total_matched)
-            db_session.add(db_market)
+
+# Catch exceptions to make sure that Betfair API and DB are closed down properly
+try:
+
+    # Get events and markets for today
+    today = datetime.date.today()
+    events = get_events(db_session, betfair_api, today)
+    markets = []
+    for event in events:
+        markets += get_markets(db_session, betfair_api, event)
+
+    # Poll each second to update market books - stop when all markets are closed
+    markets_open = len(markets)
+    while markets_open > 0:
+
+        # Scan markets to see which ones need to be updated
+        markets_open = 0
+        update_markets = []
+        for market in markets:
+
+            # Check if market is open
+            if not market.last_book or (market.last_book.status != 'CLOSED'):
+                markets_open += 1
+
+                # Get time to start and time since last poll
+                secs_to_start = (market.start_time - datetime.datetime.now()).total_seconds()
+                secs_since_last_poll = (datetime.datetime.now() - market.last_book.date_time).total_seconds() if market.last_book else 3600
+                
+                # Update market if less than 15 mins until start
+                if secs_to_start <= 900:
+                    update_markets.append(market)
+                
+                # Update market if less than 60 mins until start and more than 10 seconds since last update
+                elif (secs_to_start <= 3600) and (secs_since_last_poll > 10):
+                    update_markets.append(market)
+
+        # Update markets (this could be done in a single API call for multiple markets but
+        # that could lead to complications with amount of data and open vs closed markets)
+        for market in update_markets:
+            print(f"{datetime.datetime.now()}: Updating {market.event.name} {market.name} {market.start_time}")
+            market_book = get_market_book(db_session, betfair_api, market)
+            market.last_book_id = market_book.id
             db_session.commit()
-        for runner in market.runners:
-            print(f'    {runner.runner_name}')
-            db_runner = db_session.query(Runner).filter(Runner.betfair_id==runner.selection_id).one_or_none()
-            if not db_runner:
-                db_runner = Runner(market_id=db_market.id, betfair_id=runner.selection_id, name=runner.runner_name, sort_priority=runner.sort_priority)
-                db_session.add(db_runner)
-                db_session.commit()
+            #print(f"  {datetime.datetime.now()}: Last market book updated")
 
-# Logout from Betfair
-betfair_api.logout()
+        # Pause for a second
+        if markets_open > 0:
+            sleep(1)
 
-# Close DB session
-db_session.close()
+finally:
+    # Logout from Betfair
+    betfair_api.logout()
+
+    # Close DB session
+    db_session.close()
+
+
--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, Float, String, Date, Time, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, BigInteger, Float, String, Date, Time, DateTime, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
 
 Base = declarative_base()
@@ -11,7 +11,10 @@ class Event(Base):
     id = Column(Integer, primary_key=True)
     betfair_id = Column(String, nullable=False)
     name = Column(String, nullable=False)
+    country_code = Column(String, nullable=False)
+    time_zone = Column(String, nullable=False)
     venue = Column(String, nullable=False)
+    open_date = Column(DateTime, nullable=False)
     markets = relationship('Market', order_by='Market.start_time', backref='event')
 
 
@@ -26,6 +29,10 @@ class Market(Base):
     start_time = Column(DateTime, nullable=False)
     total_matched = Column(Float, nullable=False)
     runners = relationship('Runner', order_by='Runner.sort_priority', backref='market')
+    books = relationship('MarketBook', primaryjoin='MarketBook.market_id==Market.id', order_by='MarketBook.date_time', backref='market')
+    last_book_id = Column(Integer, ForeignKey('market_book.id'), nullable=True)
+    last_book = relationship('MarketBook', foreign_keys=[last_book_id])
+
 
 
 class Runner(Base):
@@ -34,6 +41,53 @@ class Runner(Base):
 
     id = Column(Integer, primary_key=True)
     market_id = Column(Integer, ForeignKey('market.id'), nullable=False)
-    betfair_id = Column(Integer, nullable=False)
+    betfair_id = Column(BigInteger, nullable=False)
     name = Column(String, nullable=False)
     sort_priority = Column(Integer, nullable=False)
+    #metadata
+    books = relationship('RunnerBook', backref='runner')
+
+
+class MarketBook(Base):
+
+    __tablename__ = 'market_book'
+
+    id = Column(Integer, primary_key=True)
+    market_id = Column(Integer, ForeignKey('market.id'), nullable=False)
+    date_time = Column(DateTime, nullable=False)
+    delayed = Column(Boolean)
+    status = Column(String, nullable=False)
+    bet_delay = Column(Integer, nullable=False)
+    bsp_reconciled = Column(Boolean)
+    complete = Column(Boolean)
+    inplay = Boolean()
+    number_of_winners = Column(Integer, nullable=False)
+    number_of_runners = Column(Integer, nullable=False)
+    number_of_active_runners = Column(Integer, nullable=False)
+    last_match_time = Column(DateTime, nullable=True)
+    total_matched = Column(Float, nullable=False)
+    total_available = Column(Float, nullable=False)
+    cross_matching = Boolean()
+    runners_voidable = Boolean()
+    version = Column(BigInteger, nullable=False)
+    runners = relationship('RunnerBook', backref='market_book')
+    #key_line_description
+
+class RunnerBook(Base):
+
+    __tablename__ = 'runner_book'
+
+    id = Column(Integer, primary_key=True)
+    market_book_id = Column(Integer, ForeignKey('market_book.id'), nullable=False)
+    runner_id = Column(Integer, ForeignKey('runner.id'), nullable=False)
+    handicap = Column(Float, nullable=False)
+    status = Column(String, nullable=False)
+    adjustment_factor =  Column(Float, nullable=False)
+    last_price_traded = Column(Float, nullable=True)
+    total_matched = Column(Float, nullable=True)
+    removal_date = Column(DateTime, nullable=True)
+    #starting_price
+    #exchange_prices
+    #orders
+    #matches
+    #matches_by_strategy
